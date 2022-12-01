1b0d98191b76d6bc6fdab165ffb4e054ad2753a9
V-Modder <V-Modder@web.de>
2022-12-01 17:50:12 +0100

    use label

--- server/gpu_panel.py
+++ server/gpu_panel.py
@@ -6,7 +6,6 @@ from metric.metric import GPU
 from server.gui_helper import GuiHelper
 
 class GpuPanel(QWidget):
-    __show_picture: bool
 
     def __init__(self, parent) -> None:
         super().__init__(parent)
@@ -15,6 +14,8 @@ class GpuPanel(QWidget):
         self.label_gpu_temp = GuiHelper.create_label(self, 37, 36, text="--°C")
         self.progress_gpu_load = GuiHelper.create_progressbar(self, 100, 6, 160, 20)
         self.progress_gpu_mem_load = GuiHelper.create_progressbar(self, 100, 36, 160, 20)
+        self.label_image = GuiHelper.create_label(self, 0, 0, width=self.width(), height=self.height(), image="server/resource/gpu.png")
+        self.label_image.setVisible(False)
 
         self.show_gui(False)
 
@@ -24,7 +25,7 @@ class GpuPanel(QWidget):
         self.progress_gpu_mem_load.setValue(value.memory_load)
 
     def show_gui(self, value):
-        self.__show_picture = not value
+        self.label_image.setVisible(not value)
         self.label_gpu.setVisible(value)
         self.label_gpu_temp.setVisible(value)
         self.progress_gpu_load.setVisible(value)
@@ -32,6 +33,7 @@ class GpuPanel(QWidget):
         self.repaint()
     
     def paintEvent(self, event: QPaintEvent) -> None:
+        """
         painter = QPainter(self)
         if self.__show_picture:
             image = QImage()
@@ -41,4 +43,5 @@ class GpuPanel(QWidget):
         else:
             painter.fillRect(0, 0, self.width(), self.height(), Qt.GlobalColor.transparent)
         painter.end()
+        """
         super().paintEvent(event)
