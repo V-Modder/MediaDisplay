4b7c68652e3f0e86962f2ea171441c11634e1b49
Jannik Ewers <v-modder@web.de>
2022-12-02 19:43:34 +0100

    add client check

--- server/server.py
+++ server/server.py
@@ -4,11 +4,13 @@ from flask import Flask, request
 from flask_socketio import SocketIO, Namespace
 from threading import Thread
 import traceback
+import typing
 
 from metric.metric import Metric
 
 class MetricServer(Namespace, Thread):
     _app : Flask
+    _connected_clients : typing.List[str]
 
     def __init__(self, receiver):
         super(Namespace, self).__init__()
@@ -17,6 +19,7 @@ class MetricServer(Namespace, Thread):
         socketio = SocketIO(self.__app)
         socketio.on_namespace(self)
         self.__receiver = receiver
+        self._connected_clients = []
 
     def run(self):
         print("Starting server at 0.0.0.0:5001")
@@ -29,17 +32,25 @@ class MetricServer(Namespace, Thread):
     def on_connect(self):
         print("socket connected, ", request.sid)
         print("Headers:", request.headers["Cpu-Count"])
-        try:
-            self.__receiver.restore(int(request.headers["Cpu-Count"]))
-        except Exception as e:
-            traceback.print_exc()
+        if len(self._connected_clients) > 0:
+            self.disconnect()
+        else:
+            self._connected_clients.append(request.sid)
+            try:
+                self.__receiver.restore(int(request.headers["Cpu-Count"]))
+            except Exception as e:
+                traceback.print_exc()
 
     def on_disconnect(self):
         print("socket disconnected")
-        try:
-            self.__receiver.reset()
-        except Exception as e:
-            traceback.print_exc()
+        if request.sid in self._connected_clients:    
+            print("Disconnecting, already in use")
+            self._connected_clients.remove(request.sid)
+        else:
+            try:
+                self.__receiver.reset()
+            except Exception as e:
+                traceback.print_exc()
 
     def on_get_brightness(self):
         print('received get_brightness: ')
@@ -58,7 +69,6 @@ class MetricServer(Namespace, Thread):
             traceback.print_exc()
 
     def on_metric(self, message):
-        #print('received metric: ')
         try:
             metric = Metric.deserialize(message)
             self.__receiver.receive(metric)
