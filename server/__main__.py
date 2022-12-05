d6e81f18998a37394802e574e343cea424c931b8
V-Modder <V-Modder@web.de>
2022-12-05 22:29:23 +0100

    clean

--- server/__main__.py
+++ server/__main__.py
@@ -1,21 +1,6 @@
-import logging
-from logging.handlers import RotatingFileHandler
-
 from server import pystream
 
-log_format = ('[%(asctime)s] %(levelname)-8s %(name)-12s %(message)s')
-
 def main():
-    rotatingHandler = RotatingFileHandler(filename='media_dispaly_server.log', maxBytes=1000)
-    rotatingHandler.setLevel(logging.DEBUG)
-
-    logging.basicConfig(
-		level=logging.INFO,
-      	format=log_format
-    )
-
-    logging.getLogger('').addHandler(rotatingHandler)
-
     pystream.main()
 
 main()
