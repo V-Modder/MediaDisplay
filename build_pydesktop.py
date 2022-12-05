ef91212f6aabe9c11406aa3a8f71440d81ddde82
V-Modder <V-Modder@web.de>
2022-12-05 22:18:13 +0100

    fix typo

--- build_pydesktop.py
+++ build_pydesktop.py
@@ -2,4 +2,4 @@
 
 #pyinstaller.exe -F -c -n pyDesktop -i .\pyDesktop\resource\pydesktop.ico --add-data ".\pyDesktop\resource\pydesktop.png;pydesktop\resource" --add-data ".\metric\OpenHardwareMonitorLib.dll;metric" .\media_dispaly.py
 
-#pyinstaller.exe --onedir --windowed -c -n pyDesktop -i .\pyDesktop\resource\pydesktop.ico --add-data ".\pyDesktop\resource\pydesktop.png;pydesktop\resource" --add-data ".\metric\OpenHardwareMonitorLib.dll;metric" .\media_dispaly.pyw
+#pyinstaller.exe --onedir --windowed -c -n pyDesktop -i .\pyDesktop\resource\pydesktop.ico --add-data ".\pyDesktop\resource\pydesktop.png;pydesktop\resource" --add-data ".\metric\OpenHardwareMonitorLib.dll;metric" .\media_display.pyw
