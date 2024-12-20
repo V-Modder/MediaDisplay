6de4aac8da37cbc53723cd4ae57e3cf629a530c9
Jannik Ewers <v-modder@web.de>
2024-12-20 21:07:52 +0100

    fix plaform name detection

--- server/os/platform.py
+++ server/os/platform.py
@@ -1,6 +1,7 @@
 import platform
 
+
 class Platform:
     @staticmethod
     def is_raspberry_pi() -> bool:
-        return platform.machine() == 'armv7l'
+        return platform.machine() in ['armv7l', 'aarch64']
